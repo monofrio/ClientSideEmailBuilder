d8bfb1d72506904dd8fc214b9429a47a5c04967d
Mark Onofrio <monofrio@adl.org>
2016-02-02 14:39:00 -0500

    Add Grunt function
    Added grunt function to copy over bower components files that are only
    needed for the

---
+++ gruntfile.js
@@ -0,0 +1,30 @@
+/**
+ * Created by markonofrio on 2/2/16.
+ */
+module.exports = function(grunt) {
+    // Do grunt-related things in here
+
+    grunt.initConfig({
+        copy: {
+            main:{
+                files:[
+                    //JS
+                    {src: 'bower_components/angular/angular.min.js', dest: 'dist/js', flatten: true, expand: true },
+                    {src: 'bower_components/jquery/dist/jquery.min.js', dest: 'dist/js', flatten: true, expand: true },
+                    {src: 'bower_components/bootstrap/dist/js/bootstrap.min.js', dest: 'dist/js', flatten: true, expand: true },
+                    {src: 'bower_components/angular-wysiwyg/dist/angular-wtsiwyg.min.js', dest: 'dist/js', flatten: true, expand: true },
+                    {src: 'bower_components/angular-bootstrap-colorpicker/js/bootstrap-colorpicker-module.min.js', dest: 'dist/js', flatten: true, expand: true },
+
+                    //CSS
+                    {src: 'bower_components/bootstrap/dist/css/bootstrap.min.css', dest: 'dist/css', flatten: true, expand: true },
+
+                    //FONTS
+                    {src: 'bower_components/bootstrap/dist/fonts/*', dest: 'dist/fonts', flatten: true, expand: true }
+
+                ]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('default', ['copy']);
+};
