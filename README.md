21a9b436078847fc7f7e84cde9f890795b044286
Mark Onofrio <monofrio@adl.org>
2016-02-03 17:32:04 -0500

    New Grunt Functions
    Updated with new grunt modules to email templets the easy way.

--- README.md
+++ README.md
@@ -3,15 +3,51 @@
 [![Build Status](https://travis-ci.org/monofrio/ClientSideEmailBuilder.svg?branch=Master)](https://travis-ci
 .org/monofrio/ClientSideEmailBuilder)
 
-> This is in beta.
-
-Uses:
-* Angular https://github.com/angular/angular
-    * Angular bootsrap colorpicker https://github.com/buberdds/angular-bootstrap-colorpicker
-    * Angular Wysiwyg https://github.com/TerryMooreII/angular-wysiwyg
-* Bootstrap https://github.com/twbs/bootstrap
-* jQuery https://github.com/jquery/jquery
-
+> This is in **Pre-alpha**
 
 This is designed to create simple email development tool for users with no experience with HTML.
-    
+The challenge to this application is to develope it so it doesn't have to rely on a server but uses the client side browser to create an ready to use email to be copied and pasted into an email host to send out.
+
+## Getting Started
+
+This app is using frameworks to help development and streamline the application.
+Using Twitter Bootstraps for the front-end while taken advantage of Angular MVC to bring the application together. This application is developed to be used on the client side (browser like Chrome) instead of a server.
+
+Whats being used for this project:
+* [Angular](https://angularjs.org/)
+    * [Angular Wysiwyg](https://github.com/TerryMooreII/angular-wysiwyg)
+    * [Angular Bootstrap Colorpicker](https://github.com/buberdds/angular-bootstrap-colorpicker)
+* [Bootstrap](http://getbootstrap.com/)
+* [Bower](http://bower.io/)
+* [Grunt](http://gruntjs.com/)
+* [Git](https://git-scm.com/)
+* [Jade](http://jade-lang.com/)
+* [jQuery](http://jquery.com/)
+* [Node.js](https://nodejs.org/en/)
+* [NPM](https://www.npmjs.com/)
+
+## Gathering the other assets.
+
+### Node.js
+
+You will need to install [Node.js](https://nodejs.org/en/) which will also install [NPM (Node Package Manager)](https://www.npmjs.com/) to run the commands below.
+
+### Grunt
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install by running these command:
+
+```shell
+npm install -g grunt-cli
+```
+and then run:
+
+```shell
+npm install
+```
+
+### Bower
+
+```shell
+bower install
+    ```
+
--- gruntfile.js
+++ gruntfile.js
@@ -7,28 +7,99 @@ module.exports = function(grunt) {
     grunt.initConfig({
         // Move Files into dist folder
         copy: {
-            main:{
-                files:[
-                    //HTML
-                    {src: 'jade/index.html', dest: 'dist/', flatten: true, expand:true },
+            main: {
+                files: [
+                        //HTML
+                        {src: 'jade/index.html', dest: 'dist/', flatten: true, expand: true},
 
-                    //JS
-                    {src: 'bower_components/angular/angular.min.js', dest: 'dist/js', flatten: true, expand: true },
-                    {src: 'bower_components/jquery/dist/jquery.min.js', dest: 'dist/js', flatten: true, expand: true },
-                    {src: 'bower_components/bootstrap/dist/js/bootstrap.min.js', dest: 'dist/js', flatten: true, expand: true },
-                    {src: 'bower_components/angular-wysiwyg/dist/angular-wysiwyg.min.js', dest: 'dist/js', flatten: true, expand: true },
-                    {src: 'bower_components/angular-bootstrap-colorpicker/js/bootstrap-colorpicker-module.min.js', dest: 'dist/js', flatten: true, expand: true },
+                        //JS
+                        {src: 'bower_components/angular/angular.min.js', dest: 'dist/js', flatten: true, expand: true},
+                        {src: 'bower_components/jquery/dist/jquery.min.js', dest: 'dist/js', flatten: true, expand: true},
+                        {
+                            src: 'bower_components/bootstrap/dist/js/bootstrap.min.js',
+                            dest: 'dist/js',
+                            flatten: true,
+                            expand: true
+                        },
+                        {
+                            src: 'bower_components/angular-wysiwyg/dist/angular-wysiwyg.min.js',
+                            dest: 'dist/js',
+                            flatten: true,
+                            expand: true
+                        },
+                        {
+                            src: 'bower_components/angular-bootstrap-colorpicker/js/bootstrap-colorpicker-module.min.js',
+                            dest: 'dist/js',
+                            flatten: true,
+                            expand: true
+                        },
 
-                    //CSS
-                    {src: 'bower_components/bootstrap/dist/css/bootstrap.min.css', dest: 'dist/css', flatten: true, expand: true },
+                        //CSS
+                        {
+                            src: 'bower_components/bootstrap/dist/css/bootstrap.min.css',
+                            dest: 'dist/css',
+                            flatten: true,
+                            expand: true
+                        },
 
-                    //FONTS
-                    {src: 'bower_components/bootstrap/dist/fonts/*', dest: 'dist/fonts', flatten: true, expand: true }
+                        //FONTS
+                        {src: 'bower_components/bootstrap/dist/fonts/*', dest: 'dist/fonts', flatten: true, expand: true}
 
+                    ]
+            },
+            emailDev: {
+                files: [
+                    {src: 'jade/templates/email-temp.html', dest: 'dist/templates', flatten: true, expand: true},
+                    {src: 'bower_components/ink/css/ink.css', dest: 'dist/templates/css', flatten: true, expand: true},
+                    {src: 'sass/temp-style.css', dest: 'dist/templates/css', flatten: true, expand: true}
                 ]
             }
+        },
+
+        // Remove CSS that is not being used
+        uncss: {
+            dist: {
+                 src: ['dist/templates/email-temp.html'],
+                 dest: 'dist/templates/css/tidy.css',
+                 options: {
+                     report: 'min' // optional: include to report savings
+                }
+            }
+        },
+
+        // Replace all the development CSS files with the distribution
+        processhtml: {
+            dist: {
+                files: {
+                    'dist/templates/email-temp.html': ['dist/templates/email-temp.html']
+                }
+            }
+        },
+
+        // Inlines CSS while leaving the media queries
+        premailer: {
+            main: {
+                options: {
+                    verbose: true
+                },
+                files: {
+                    'dist/templates/email-temp-inline.html': ['dist/templates/email-temp.html']
+                }
+            }
         }
+
     });
+
+
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.registerTask('default', ['copy']);
+    grunt.loadNpmTasks('grunt-uncss');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-premailer');
+
+    var target = grunt.option('target') || 'main';
+
+    grunt.registerTask('email', ['copy']);
+    grunt.registerTask('body', ['copy:' + target]);
+    grunt.registerTask('inline', ['uncss', 'processhtml', 'premailer']);
+
 };
